b092380cadcdb32d3a2fcb680ae51787ff48e90b
Bohdan Serdinov <67954636+bogdanserdinov@users.noreply.github.com>
2023-06-06 04:27:54 +0300

    changed get profile method (#3)

--- gen/go/profile/v1/service.pb.gw.go
+++ gen/go/profile/v1/service.pb.gw.go
@@ -168,9 +168,20 @@ func local_request_ProfileService_UpdateProfile_0(ctx context.Context, marshaler
 
 }
 
+var (
+	filter_ProfileService_GetProfileByDID_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_ProfileService_GetProfileByDID_0(ctx context.Context, marshaler runtime.Marshaler, client ProfileServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq emptypb.Empty
+	var protoReq GetProfileByDIDRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ProfileService_GetProfileByDID_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.GetProfileByDID(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -178,8 +189,15 @@ func request_ProfileService_GetProfileByDID_0(ctx context.Context, marshaler run
 }
 
 func local_request_ProfileService_GetProfileByDID_0(ctx context.Context, marshaler runtime.Marshaler, server ProfileServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq emptypb.Empty
+	var protoReq GetProfileByDIDRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ProfileService_GetProfileByDID_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.GetProfileByDID(ctx, &protoReq)
 	return msg, metadata, err
